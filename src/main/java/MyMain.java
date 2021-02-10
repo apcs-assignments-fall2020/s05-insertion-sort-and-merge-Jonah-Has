e76dbeab90e905f61a7138d48a7eec4486ca71b3
Jonah-Has <23jhasegawa@athenian.org>
2021-02-10 02:25:37 +0000

    wrote the code for insertionsort

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,9 +4,28 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        return isr(list, 0);
     }
+    public static ArrayList<Integer> isr(ArrayList<Integer> list, int i){ 
+        
+        if (i >= list.size() - 1){
+            return list;
+        }
+        if (i < 0){
+            return isr(list, 0);
+        }      
+        
+        if (list.get(i) <= list.get(i + 1)){
+            return isr(list, i + 1);
+        }
+        else{
+            int temp = list.get(i);
+            list.set(i, list.get(i + 1));
+            list.set(i + 1, temp);
+            return isr(list, i - 1);
+        }
+        
+     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
